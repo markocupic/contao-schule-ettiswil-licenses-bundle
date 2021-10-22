eb4df8e2604e90c3ab19c33abe14ff9232f6ff8c
Marko Cupic <m.cupic@gmx.ch>
2021-10-22 19:41:08 +0200

    Update tl_schule_ettiswil_licenses.php

--- src/Resources/contao/dca/tl_schule_ettiswil_licenses.php
+++ src/Resources/contao/dca/tl_schule_ettiswil_licenses.php
@@ -8,7 +8,6 @@
  * @license MIT
  */
 
-use Contao\Backend;
 use Contao\Input;
 use Contao\System;
 use Markocupic\ContaoSchuleEttiswilLicensesBundle\ExportData\Excel;
@@ -16,160 +15,161 @@ use Markocupic\ContaoSchuleEttiswilLicensesBundle\ExportData\Excel;
 /**
  * Table tl_schule_ettiswil_licenses
  */
-$GLOBALS['TL_DCA']['tl_schule_ettiswil_licenses'] = [
+$GLOBALS['TL_DCA']['tl_schule_ettiswil_licenses'] = array(
     // Config
-    'config'      => [
+    'config'      => array(
         'dataContainer'    => 'Table',
         'enableVersioning' => true,
-        'sql'              => [
-            'keys' => [
+        'sql'              => array(
+            'keys' => array(
                 'id' => 'primary',
-            ],
-        ],
-        'onload_callback'  => [
-            ['tl_schule_ettiswil_licenses', 'route'],
-        ],
-    ],
-    'list'        => [
-        'sorting'           => [
+            ),
+        ),
+        'onload_callback'  => array(
+            array('tl_schule_ettiswil_licenses', 'route'),
+        ),
+    ),
+    'list'        => array(
+        'sorting'           => array(
             'mode'        => 2,
-            'fields'      => ['title', 'userid', 'passphrase', 'expirationdate', 'department', 'topic'],
+            'fields'      => array('title', 'userid', 'passphrase', 'expirationdate', 'department', 'topic'),
             'flag'        => 1,
             'panelLayout' => 'filter;sort,search,limit',
-        ],
-        'label'             => [
-            'fields' => ['title'],
+        ),
+        'label'             => array(
+            'fields' => array('title'),
             'format' => '%s',
-        ],
-        'global_operations' => [
-            'all'         => [
+        ),
+        'global_operations' => array(
+            'all'         => array(
                 'label'      => &$GLOBALS['TL_LANG']['MSC']['all'],
                 'href'       => 'act=select',
                 'class'      => 'header_edit_all',
                 'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
-            ],
-            'excelExport' => [
+            ),
+            'excelExport' => array(
                 'label'      => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['excelExport'],
                 'href'       => 'action=excelExport',
                 'class'      => 'header_icon header_excel_export',
                 'icon'       => 'bundles/markocupiccontaoschuleettiswillicenses/excel.svg',
                 'attributes' => 'onclick="Backend.getScrollOffset();" accesskey="i"',
-            ],
-        ],
-        'operations'        => [
-            'edit'   => [
+            ),
+        ),
+        'operations'        => array(
+            'edit'   => array(
                 'label' => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['edit'],
                 'href'  => 'act=edit',
                 'icon'  => 'edit.gif',
-            ],
-            'copy'   => [
+            ),
+            'copy'   => array(
                 'label' => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['copy'],
                 'href'  => 'act=copy',
                 'icon'  => 'copy.gif',
-            ],
-            'delete' => [
+            ),
+            'delete' => array(
                 'label'      => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
-            ],
-            'show'   => [
+                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
+            ),
+            'show'   => array(
                 'label'      => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['show'],
                 'href'       => 'act=show',
                 'icon'       => 'show.gif',
                 'attributes' => 'style="margin-right:3px"',
-            ],
-        ],
-    ],
+            ),
+        ),
+    ),
     // Palettes
-    'palettes'    => [
-        '__selector__' => ['addSubpalette'],
+    'palettes'    => array(
+        '__selector__' => array('addSubpalette'),
         'default'      => '{title_legend},title,userid,passphrase,notice;{expirationDate_legend},expirationdate;{config_legend},department,topic',
-    ],
+    ),
     // Subpalettes
-    'subpalettes' => [
+    'subpalettes' => array(
         'addSubpalette' => 'textareaField',
-    ],
+    ),
     // Fields
-    'fields'      => [
-        'id'             => [
+    'fields'      => array(
+        'id'             => array(
             'sql' => 'int(10) unsigned NOT NULL auto_increment',
-        ],
-        'tstamp'         => [
+        ),
+        'tstamp'         => array(
             'sql' => 'int(10) unsigned NOT NULL default ""',
-        ],
-        'title'          => [
+        ),
+        'title'          => array(
             'inputType' => 'text',
             'exclude'   => true,
             'search'    => true,
             'filter'    => true,
             'sorting'   => true,
             'flag'      => 1,
-            'eval'      => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'clr'],
+            'eval'      => array('mandatory' => true, 'maxlength' => 255, 'tl_class' => 'clr'),
             'sql'       => "varchar(255) NOT NULL default ''",
-        ],
-        'userid'         => [
+        ),
+        'userid'         => array(
             'inputType' => 'text',
             'exclude'   => true,
             'search'    => true,
             'filter'    => true,
             'sorting'   => true,
             'flag'      => 1,
-            'eval'      => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'eval'      => array('mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'       => "varchar(255) NOT NULL default ''",
-        ],
-        'passphrase'     => [
+        ),
+        'passphrase'     => array(
             'inputType' => 'text',
             'exclude'   => true,
             'search'    => true,
             'filter'    => true,
             'sorting'   => true,
             'flag'      => 1,
-            'eval'      => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+            'eval'      => array('mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'       => "varchar(255) NOT NULL default ''",
-        ],
-        'notice'         => [
+        ),
+        'notice'         => array(
             'inputType' => 'textarea',
+            'default'   => '',
             'exclude'   => true,
             'search'    => true,
-            'eval'      => ['tl_class' => 'clr'],
-            'sql'       => "text NOT NULL default ''",
-        ],
-        'department'     => [
+            'eval'      => array('tl_class' => 'clr'),
+            'sql'       => "text NULL"
+        ),
+        'department'     => array(
             'inputType' => 'select',
             'exclude'   => true,
             'search'    => true,
             'filter'    => true,
             'sorting'   => true,
             'reference' => $GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses'],
-            'options'   => ['kg', 'ps', 'iss', 'if', 'all'],
-            'eval'      => ['mandatory' => true, 'includeBlankOption' => false, 'tl_class' => 'w50'],
+            'options'   => array('kg', 'ps', 'iss', 'if', 'all'),
+            'eval'      => array('mandatory' => true, 'includeBlankOption' => false, 'tl_class' => 'w50'),
             'sql'       => "varchar(255) NOT NULL default ''",
-        ],
-        'topic'          => [
+        ),
+        'topic'          => array(
             'inputType' => 'select',
             'exclude'   => true,
             'search'    => true,
             'filter'    => true,
             'sorting'   => true,
             'reference' => $GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses'],
-            'options'   => ['mt', 'de', 'en', 'fr', 'rzg', 'lk', 'pu', 'bus', 'nat', 'mu', 'mui', 'bg', 'ttg', 'misc'],
-            'eval'      => ['mandatory' => true, 'includeBlankOption' => false, 'tl_class' => 'w50'],
+            'options'   => array('mt', 'de', 'en', 'fr', 'rzg', 'lk', 'pu', 'bus', 'nat', 'mu', 'mui', 'bg', 'ttg', 'misc'),
+            'eval'      => array('mandatory' => true, 'includeBlankOption' => false, 'tl_class' => 'w50'),
             'sql'       => "varchar(255) NOT NULL default ''",
-        ],
-        'expirationdate' => [
+        ),
+        'expirationdate' => array(
             'exclude'   => true,
             'inputType' => 'text',
-            'eval'      => ['rgxp' => 'date', 'datepicker' => true, 'tl_class' => 'w50 wizard'],
+            'eval'      => array('rgxp' => 'date', 'datepicker' => true, 'tl_class' => 'w50 wizard'),
             'sql'       => "varchar(255) NOT NULL default ''",
-        ],
-    ],
-];
+        ),
+    ),
+);
 
 /**
  * Class tl_schule_ettiswil_licenses
  */
-class tl_schule_ettiswil_licenses extends Backend
+class tl_schule_ettiswil_licenses
 {
     /**
      * tl_schule_ettiswil_licenses constructor.
