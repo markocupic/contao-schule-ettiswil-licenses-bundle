a24a14e05fd608b9e106031c0c41db4649ea341f
Marko Cupic <m.cupic@gmx.ch>
2022-07-27 13:34:46 +0200

    Fix undefined array key issue

--- src/Resources/contao/dca/tl_schule_ettiswil_licenses.php
+++ src/Resources/contao/dca/tl_schule_ettiswil_licenses.php
@@ -70,7 +70,7 @@ $GLOBALS['TL_DCA']['tl_schule_ettiswil_licenses'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'   => [
                 'label'      => &$GLOBALS['TL_LANG']['tl_schule_ettiswil_licenses']['show'],
